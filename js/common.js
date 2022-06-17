d214cee47a7351c6f66234f701b3968827e896c0
yoonme <yoonmem86@gmail.com>
2022-06-17 09:12:56 +0630

    Add the textarea box

--- js/common.js
+++ js/common.js
@@ -1,4 +1,3 @@
-console.log("true");
 $(document).ready(function(){
   var testObject = {};
   var addLocal = 0;
@@ -7,16 +6,32 @@ $(document).ready(function(){
       let newName = ("name" + addLocal);
        console.log(newName);
       var submit = $("#txt_area").val();
-      testObject[newName] = submit;
-       console.log(testObject);
-      //  var local = localStorage.setItem("name" : testObject);
-       localStorage.setItem('name', JSON.stringify(testObject));
-      //  var local = localStorage.getItem(testObject);
+      testObject[newName]= submit;
+      console.log(testObject);
+      localStorage.setItem('name', JSON.stringify(testObject));
        if(submit != ""){
-          $(".appendAdd").append(`<textarea class="" style="border:1px solid;width:300px;height: 200px;margin-top: 20px;margin-left: 20px;">${submit}</textarea>`);
+         var SubButton = `<div id="addbtnSpot"><textarea class="" style="border:1px solid;width:300px;height: 200px;padding: 20px;margin-top: 20px;margin-left: 20px;position:relative;">${submit}</textarea><div class="spotImg"></div><button id = 'dlt_btn' type='button'>Delete</button></div>`;
+          $(".appendAdd").append(SubButton);
         }else{
             console.log(false);
         };
         $("#txt_area").val('');
       });
+      function get(){
+        var getJson = localStorage.getItem('name')
+        if(getJson){
+            arrayName = JSON.parse(getJson);
+        }
+        console.log(getJson);
+    } 
+    get();
+    console.log(arrayName);
+    Object.entries(arrayName).forEach(([key, value]) => {
+      console.log(`${key}: ${value}`)
+      $(".appendAdd").append(`<div id="addbtnSpot"><textarea class="" style="border:1px solid;width:300px;height: 200px;padding: 20px;margin-top: 20px;margin-left: 20px;">${value}</textarea><div class="spotImg"></div><button id = 'dlt_btn' type='button'>Delete</button></div>`);
+      get();
+  });
+  // $(".spotImg").click(function(){
+  //   $("#dlt_btn").show();
+  // });
 });
